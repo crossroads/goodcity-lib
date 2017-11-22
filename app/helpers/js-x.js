2cda5025b88fa9f23c72e1ea981f5477542d10a8
namrataukirde <namrata.ukirde@kiprosh.com>
2017-11-22 21:31:19 +0530

    fixed warning - missing semicolon

--- app/helpers/js-x.js
+++ app/helpers/js-x.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function jsX(params) {
   var paramNames = params.slice(1).map(function(val, idx) { return "p" + idx; });
-  var func = Function.apply(this, paramNames.concat("return " + params[0] + ";"))
+  var func = Function.apply(this, paramNames.concat("return " + params[0] + ";"));
   return func.apply(params[1] === undefined ? this : params[1], params.slice(1));
 }
 
