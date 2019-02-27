0a08fb1aed6fd873eaa1f5446cf2285bced44265
Abhinav Garg <abhinav.g@kiprosh.com>
2019-02-27 17:59:37 +0530

    remove common component

--- app/helpers/apply-line-break.js
+++ app/helpers/apply-line-break.js
@@ -1,7 +1,2 @@
-import Ember from "ember";
-
-export default Ember.Helper.helper(function (value) {
-  value = Ember.Handlebars.Utils.escapeExpression(value || "");
-  value = value.replace(/(\r\n|\n|\r)/gm, '<br>');
-  return new Ember.String.htmlSafe(value);
-});
+import applyLineBreak from 'goodcity-lib/helpers/apply-line-break';
+export default applyLineBreak;
--- app/models/sender.js
+++ app/models/sender.js
@@ -1,16 +1,2 @@
-import Ember from 'ember';
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
-
-export default Model.extend({
-  firstName: attr('string'),
-  lastName: attr('string'),
-  mobile: attr('string'),
-  createdAt: attr('date'),
-  email: attr('string'),
-  title: attr('string'),
-
-  fullName: Ember.computed('firstName', 'lastName', function () {
-    return (this.get("title") + " " + this.get('firstName') + " " + this.get('lastName'));
-  }),
-});
+import sender from 'goodcity-lib/models/sender';
+export default sender;
