c0a5fae93b34873fe1b6395d7dd1e891a2486932
namrataukirde <namrata.ukirde@kiprosh.com>
2019-03-26 22:15:13 +0530

    send language with headers

--- app/utils/ajax-promise.js
+++ app/utils/ajax-promise.js
@@ -1,27 +1,37 @@
 import Ember from 'ember';
 import config from '../config/environment';
 
-export default function(url, type, authToken, data, args) {
-  return new Ember.RSVP.Promise(function(resolve, reject) {
+export default function (url, type, authToken, data, args, language = "en") {
+  return new Ember.RSVP.Promise(function (resolve, reject) {
     var headers = {
+      "Accept-Language": language,
       "X-GOODCITY-APP-NAME": config.APP.NAME,
       "X-GOODCITY-APP-VERSION": config.APP.VERSION,
       "X-GOODCITY-APP-SHA": config.APP.SHA
     };
-    if(authToken) {
-      headers = Ember.$.extend(headers, { Authorization: "Bearer " + authToken });
+    if (authToken) {
+      headers = Ember.$.extend(headers, {
+        Authorization: "Bearer " + authToken
+      });
     }
 
     Ember.$.ajax(Ember.$.extend({}, {
       type: type,
       dataType: "json",
       data: data,
+      language: language,
       url: url.indexOf('http') === -1 ? config.APP.SERVER_PATH + url : url,
       headers: headers,
-      success: function(data) { Ember.run(function() { resolve(data); }); },
-      error: function(jqXHR) {
+      success: function (data) {
+        Ember.run(function () {
+          resolve(data);
+        });
+      },
+      error: function (jqXHR) {
         jqXHR.url = url;
-        Ember.run(function() { reject(jqXHR); });
+        Ember.run(function () {
+          reject(jqXHR);
+        });
       }
     }, args));
   });
