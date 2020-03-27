0d0fe79b784d5bc2131cde161338668865a67457
shreyas098 <shreyash@kiprosh.com>
2020-03-27 14:02:55 +0530

    0 can be entered now in labels column in item creation

--- addon/components/numeric-input.js
+++ addon/components/numeric-input.js
@@ -1,13 +1,25 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.TextField.extend({
   tagName: "input",
   type: "tel",
-  attributeBindings: [ "name", "type", "value", "maxlength", "id", "autoFocus" , "placeholder", "required", "pattern"],
+  attributeBindings: [
+    "name",
+    "type",
+    "value",
+    "maxlength",
+    "id",
+    "autoFocus",
+    "placeholder",
+    "required",
+    "pattern"
+  ],
   classNameBindings: ["class"],
 
   didInsertElement() {
-    if(this.attrs.autoFocus) { this.$().focus(); }
+    if (this.attrs.autoFocus) {
+      this.$().focus();
+    }
   },
 
   currentKey: Ember.computed({
@@ -20,39 +32,46 @@ export default Ember.TextField.extend({
   }),
 
   focusOut() {
-    if(parseInt(this.get("value"), 10) === 0) {
+    if (parseInt(this.get("value"), 10) === 0 && this.get("name") != "labels") {
       this.set("value", null);
     }
   },
 
-  isAllowed: Ember.computed('currentKey', function(){
-    var key = this.get('currentKey');
-    var allowed = (key === 13 ||
+  isAllowed: Ember.computed("currentKey", function() {
+    var key = this.get("currentKey");
+    var allowed =
+      key === 13 ||
       key === 8 ||
       key === 9 ||
       key === 46 ||
       key === 39 ||
-      (key >= 35 && key <= 37));
+      (key >= 35 && key <= 37);
     return allowed;
   }),
 
-  keyUp: function(){
+  keyUp: function() {
     var value = this.attrs.value.value;
     var regexPattern = new RegExp("^".concat(this.attrs.pattern, "$"));
-    if(value && value.toString().search(regexPattern) !== 0){
-      this.set('value', value.replace(/\D/g,''));
+    if (value && value.toString().search(regexPattern) !== 0) {
+      this.set("value", value.replace(/\D/g, ""));
     }
     return true;
   },
 
   whichKey(e, key) {
     var keyList = [13, 8, 9, 39, 46];
-    return ((e.ctrlKey && key === 86) || (keyList.indexOf(key) >= 0) || (key >= 35 && key <= 37) || (key >= 48 && key <= 57) || (key >= 96 && key <= 105));
+    return (
+      (e.ctrlKey && key === 86) ||
+      keyList.indexOf(key) >= 0 ||
+      (key >= 35 && key <= 37) ||
+      (key >= 48 && key <= 57) ||
+      (key >= 96 && key <= 105)
+    );
   },
 
   keyDown: function(e) {
     var key = e.charCode || e.keyCode || 0;
-    this.set('currentKey', key);
+    this.set("currentKey", key);
 
     // allow ctrl+v, enter, backspace, tab, delete, numbers, keypad numbers
     // home, end only.
@@ -62,6 +81,8 @@ export default Ember.TextField.extend({
 
   keyPress: function() {
     var inputValue = (this.value || "").toString();
-    return this.get('isAllowed') ? true : (inputValue.length < this.attrs.maxlength);
+    return this.get("isAllowed")
+      ? true
+      : inputValue.length < this.attrs.maxlength;
   }
 });
